a126e4fd88310943d44ac6e0ce5ecbe5192b721e
Mukesh Roy <mukesh.raj60@gmail.com>
2024-10-15 19:07:46 +0530

    Updated the basics setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./config/config");
+const app = require("./app");
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => console.log("MONGO DB Connected"))
-  .catch((error) => console.error("Couldn't Connect mongo DB"));
-
-
-  
+  .then(() => {
+    console.log("MONGODB Connected...");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
+    });
+  })
+  .catch((error) => console.error("Couldn't Connect to MongoDB..."));
