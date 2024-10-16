3b867ebc52b31f81faa323a3018d8980b3396156
Mukesh Roy <mukesh.raj60@gmail.com>
2024-10-16 23:38:46 +0530

    Created post Concepts for social media

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,8 +5,8 @@ const postRouter = express.Router();
 
 postRouter.post("/", auth.checkAuth, postController.createPost);
 postRouter.get("/", auth.checkAuth, postController.getAllPost);
-postRouter.get("/", auth.checkAuth, postController.getPostById);
-postRouter.put("/", auth.checkAuth, postController.updatePost);
-postRouter.delete("/", auth.checkAuth, postController.deletePost);
+postRouter.get("/:id", auth.checkAuth, postController.getPostById);
+postRouter.put("/:id", auth.checkAuth, postController.updatePost);
+postRouter.delete("/:id", auth.checkAuth, postController.deletePost);
 
 module.exports = postRouter;
