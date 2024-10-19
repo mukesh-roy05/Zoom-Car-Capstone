b9b9921d6df87d7e0059e3bf83b285c5afd05c1b
Mukesh Roy <mukesh.raj60@gmail.com>
2024-10-20 01:13:43 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
-
-import { defineConfig } from "vite";
-
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        "react-icons/gi", // Correct casing
-        "react-icons/im",
-        "react-icons/io5",
-        "react-icons/md",
-        "react-icons/cg",
-        "react-icons/ai",
-      ],
-    },
-  },
+  plugins: [react()],
 });
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       external: [
+//         "react-icons/gi", // Correct casing
+//         "react-icons/im",
+//         "react-icons/io5",
+//         "react-icons/md",
+//         "react-icons/cg",
+//         "react-icons/ai",
+//       ],
+//     },
+//   },
+// });
