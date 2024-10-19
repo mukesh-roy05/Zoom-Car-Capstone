df392412dc16eef040e326fc4085a58e31e445e6
Mukesh Roy <mukesh.raj60@gmail.com>
2024-10-20 00:31:55 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()]
 // })
 
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["react-icons"],
+      external: ["react-icons/gi"], // only if absolutely needed
     },
   },
-};
+});
