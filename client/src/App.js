6b29c842896407fdf463c36845de64bfe2e104d8
Mukesh Roy <mukesh.raj60@gmail.com>
2024-11-13 23:51:31 +0530

    Updated APP and Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BookingCar from "./pages/BookingCar";
-import Spinner from "./components/Spinner";
+import "./antd.css";
+import UserBookings from "./pages/UserBookings";
+import AddCar from "./pages/AddCar";
+import AdminHome from "./pages/AdminHome";
+import EditCar from "./pages/EditCar";
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ProtectedRoute element={<Home />} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/bookingCar" element={<BookingCar />} />
+          <Route
+            path="/booking/:carid"
+            element={<ProtectedRoute element={<BookingCar />} />}
+          />
+          <Route
+            path="/userbookings"
+            element={<ProtectedRoute element={<UserBookings />} />}
+          />
+          <Route
+            path="/addcar"
+            element={<ProtectedRoute element={<AddCar />} />}
+          />
+          <Route
+            path="/editcar/:carid"
+            element={<ProtectedRoute element={<EditCar />} />}
+          />
+          <Route
+            path="/admin"
+            element={<ProtectedRoute element={<AdminHome />} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
+
+function ProtectedRoute({ element }) {
+  if (localStorage.getItem("user")) {
+    return element;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
---
+++ client/src/pages/EditCar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditCar = () => {
+  return <div>EditCar</div>;
+};
+
+export default EditCar;
