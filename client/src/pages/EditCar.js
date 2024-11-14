ff56eb028aa8918fa3d01896191c01f1bd91abaf
Mukesh Roy <mukesh.raj60@gmail.com>
2024-11-14 12:01:52 +0530

    Updated editCar

--- client/src/pages/EditCar.js
+++ client/src/pages/EditCar.js
@@ -4,18 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import DefaultLayout from "../components/DefaultLayout";
 import Spinner from "../components/Spinner";
 import { addCar, editCar, getAllCars } from "../redux/actions/carsActions";
+import { useParams } from "react-router-dom";
+
 function EditCar({ match }) {
   const { cars } = useSelector((state) => state.carsReducer);
   const dispatch = useDispatch();
   const { loading } = useSelector((state) => state.alertsReducer);
   const [car, setcar] = useState();
   const [totalcars, settotalcars] = useState([]);
+  const { carid } = useParams();
+
   useEffect(() => {
     if (cars.length == 0) {
       dispatch(getAllCars());
     } else {
       settotalcars(cars);
-      setcar(cars.find((o) => o._id == match.params.carid));
+      setcar(cars.find((o) => o._id == carid));
       console.log(car);
     }
   }, [cars]);
